278438f9839f132e730c052312851fc6e7998919
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-12-03 12:36:35 +0200

    PermutationsWithoutRepetition solution

---
+++ combinatorial-algoritms/src/PermutationsWithoutRepetition.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PermutationsWithoutRepetition {
+
+    public static String[] elements;
+    public static String[] permutes;
+    public static boolean[] used;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        elements = scanner.nextLine().split(" ");
+
+        permutes = new String[elements.length];
+        used = new boolean[elements.length];
+
+        permute(0);
+    }
+
+    private static void permute(int index) {
+        if(index == elements.length) {
+            print();
+            return;
+        } else {
+            for(int i = 0; i < elements.length; i++) {
+                if(!used[i]) {
+                    used[i] = true;
+                    permutes[index] = elements[i];
+                    permute(index + 1);
+                    used[i] = false;
+                }
+
+            }
+        }
+    }
+
+    public static void print() {
+        System.out.println(String.join(" ", permutes));
+    }
+}
