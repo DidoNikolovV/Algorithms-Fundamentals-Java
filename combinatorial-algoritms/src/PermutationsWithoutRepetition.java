497e25c08ca28816c9bae56d33945a2d8d95f7d2
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-12-03 13:04:37 +0200

    Permutations optimization using "Swap Algorithm"

--- combinatorial-algoritms/src/PermutationsWithoutRepetition.java
+++ combinatorial-algoritms/src/PermutationsWithoutRepetition.java
@@ -1,40 +1,36 @@
 import java.util.Scanner;
 
 public class PermutationsWithoutRepetition {
-
     public static String[] elements;
-    public static String[] permutes;
-    public static boolean[] used;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         elements = scanner.nextLine().split(" ");
 
-        permutes = new String[elements.length];
-        used = new boolean[elements.length];
-
         permute(0);
     }
 
     private static void permute(int index) {
         if(index == elements.length) {
-            print();
+            print(elements);
             return;
-        } else {
-            for(int i = 0; i < elements.length; i++) {
-                if(!used[i]) {
-                    used[i] = true;
-                    permutes[index] = elements[i];
-                    permute(index + 1);
-                    used[i] = false;
-                }
-
-            }
         }
+        permute(index + 1);
+            for(int i = index + 1; i < elements.length; i++) {
+                swap(elements, index, i);
+                permute(index + 1);
+                swap(elements, index, i);
+            }
     }
 
-    public static void print() {
-        System.out.println(String.join(" ", permutes));
+    private static void swap(String[] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
     }
 }
