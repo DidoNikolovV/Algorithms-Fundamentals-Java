4efcd36e1b4707db9935573ef20681beed9ef92e
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-12-08 07:41:44 +0200

    Shortest path in graph

--- Graph-Theory-Lab/src/main/java/Main.java
+++ Graph-Theory-Lab/src/main/java/Main.java
@@ -4,34 +4,81 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    public static boolean[] visited;
+    public static int[] prevNodes;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+
         List<List<Integer>> graph = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            String nextLine = scanner.nextLine();
-            if(nextLine.trim().equals("")) {
-                graph.add(new ArrayList<>());
-            } else {
-                List<Integer> nextNodes = Arrays.stream(nextLine.split("\\s+"))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-                graph.add(nextNodes);
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int edges = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < edges; i++) {
+            int[] paths = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            graph.get(paths[0]).add(paths[1]);
+        }
+
+        int source = Integer.parseInt(scanner.nextLine());
+        int destination = Integer.parseInt(scanner.nextLine());
+
+        visited = new boolean[n + 1];
+        prevNodes = new int[n + 1];
+
+        Arrays.fill(prevNodes, -1);
+
+        bfs(graph, source, destination);
+
+        List<Integer> path = new ArrayList<>();
+
+        path.add(destination);
+
+        int prevNode = prevNodes[destination];
+
+        while(prevNode != -1) {
+            path.add(prevNode);
+            prevNode = prevNodes[prevNode];
+        }
+
+
+        System.out.println("Shortest path length is: " + (path.size() - 1));
+        for (int i = path.size() - 1; i >= 0 ; i--) {
+            System.out.print(path.get(i) + " ");
+        }
+    }
+
+    private static void bfs(List<List<Integer>> graph, int source, int destination) {
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(source);
+        visited[source] = true;
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            if(node == destination) {
+                return;
+            }
+
+            for (int child : graph.get(node)) {
+                if(!visited[child]) {
+                    visited[child] = true;
+                    prevNodes[child] = node;
+                    queue.offer(child);
+
+                }
             }
         }
 
-        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
-
-        for(Deque<Integer> connectedComponent : connectedComponents) {
-            System.out.print("Connected component: ");
-            for (int integer : connectedComponent) {
-                System.out.print(integer + " ");
-            }
-
-            System.out.println();
-        }
     }
 
     public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
@@ -67,19 +114,19 @@ public class Main {
       }
     }
 
-    private static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
-        if(!visited[node]) {
-            visited[node] = true;
-
-            for(int child : graph.get(node)) {
-                dfs(child, components, graph, visited);
-            }
-
-            components.get(components.size() - 1).offer(node);
-        }
-
-
-    }
+//    private static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+//        if(!visited[node]) {
+//            visited[node] = true;
+//
+//            for(int child : graph.get(node)) {
+//                dfs(child, components, graph, visited);
+//            }
+//
+//            components.get(components.size() - 1).offer(node);
+//        }
+//
+//
+//    }
 
     public static Collection<String> topSort(Map<String, List<String>> graph) {
         Map<String, Integer> dependenciesCount = getDependenciesCount(graph);
